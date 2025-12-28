b9d94b324a8ae7fae066ee1d78c845ce506ea726
Onur Işık <onurisik5523@gmail.com>
2025-12-28 14:07:36 +0300

    chore: configure frontend types, css imports and disable strict mode

---
+++ frontend/src/types/react-form-builder2.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-form-builder2' {
+    const ReactFormBuilder: any;
+    const ElementStore: any;
+    const ReactFormGenerator: any;
+    export { ReactFormBuilder, ElementStore, ReactFormGenerator };
+}
